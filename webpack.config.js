9266b0201ab94713d8568a2b441d42161c25bfea
Kevin O'Neil <kevinmoneil@me.com>
2017-12-20 19:37:28 +0900

    adds basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.wasm$/,
+        loaders: ["wasm-loader"]
+      }
+    ]
+  }
+};
